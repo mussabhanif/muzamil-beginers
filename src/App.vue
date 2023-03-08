52b4810afcec18d8ece50831774856aedd1d6d67
musab <muhammadmussab156@gmail.com>
2023-03-08 10:04:17 +0500

    slots class

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,17 @@ import BaseLayout from "@/components/layout/BaseLayout.vue";
     <template v-slot:header>
       <BaseButton title="Logout" icon="logout" />
     </template>
-
+    <!-- content -->
+    <div class="flex flex-col gap-4">
+      <h3 class="text-3xl">
+        This is baselayout with named slots. this is our main content
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis itaque debitis
+        corporis delectus aliquid! Enim eaque labore distinctio consequatur quibusdam
+        molestias officiis laboriosam vitae omnis ea aut asperiores, inventore beatae.
+      </p>
+    </div>
     <!--footer -->
     <template v-slot:footer>
       <div class="bg-green-500 py-2">
