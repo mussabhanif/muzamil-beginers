cca4b138f650af77f196af30365d53938758ec20
musab <muhammadmussab156@gmail.com>
2023-03-10 19:29:25 +0500

    reactive class

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,28 @@
 <script setup>
-import BaseButton from "@/components/BaseButton.vue";
-import BaseCard from "@/components/BaseCard.vue";
-import BaseLayout from "@/components/layout/BaseLayout.vue";
-import Clock from "./Clock.vue";
-import { ref } from "vue";
+import { reactive } from "vue";
 
-const name = ref("");
-const check = () => {
-  console.log(name.value);
-}
+const state = reactive({
+  name: "Musab",
+  email: "musab@example.com",
+  mybool: true,
+  myNum: 1234,
+  myNull: null,
+  myArr: [
+    {
+      name: "dashrat",
+      emai: "dashrat@karimapps.com",
+    },
+  ],
+  user: {
+    name: "Muzamil",
+    email: "Muzamil@g.com",
+  },
+});
+const arr = [1, 87878, 3, 4, 5, "musab"];
 </script>
 
 <template>
-  <BaseLayout>
-    <!-- temp:header -->
-    <template v-slot:header>
-      <BaseButton title="Logout" icon="logout" />
-    </template>
-    <!-- content -->
-    <!-- <Clock /> -->
-    <div class="flex flex-col gap-4 w-screen items-center">
-      <div>
-        <label for="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Name"
-          v-model="name"
-          class="block w-80 flex-1 rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset focus:outline-none ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        />
-      </div>
-      {{ name }}
-      <BaseButton title="Submit" v-on:click="check"/>
-    </div>
-
-    <!--footer -->
-    <!-- <template v-slot:footer>
-      <div class="bg-green-500 py-2">
-        <h3 class="text-3xl">Hi i am footer</h3>
-      </div>
-    </template> -->
-  </BaseLayout>
+  <div>{{ state.myArr[0]?.name }}</div>
 </template>
 
 <style scoped></style>
--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,23 +1,11 @@
 <script setup>
-const props = defineProps({
-  title: String,
-  icon: String,
-});
-const check = () => {
-  alert(props?.title);
-};
-// defineProps(["title"]);
+defineProps(["title", "type"]);
 </script>
-
 <template>
   <button
-    class="bg-green-500 py-2 px-3 text-white rounded-lg hover:bg-green-600 flex items-center justify-center"
+    v-bind:type="type"
+    class="inline-flex items-center px-4 py-2 rounded-md font-semibold tracking-widest focus:outline-none focus:[#23a100] focus:shadow-outline-gray transition ease-in-out duration-150 bg-green-400 hover:bg-green-500 active:bg-bg-green-600 text-white"
   >
-    <span v-if="icon" class="material-symbols-outlined"> {{ icon }} </span>
-
-    <span v-if="title"> {{ title }}</span>
-    <span>
-      <slot />
-    </span>
+    {{ title }}
   </button>
 </template>
